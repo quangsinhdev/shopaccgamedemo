b6a1bdc9734e52963a9e84b83bb0e3820229d28b
Quang Sinh <77193639+quangsinhdev@users.noreply.github.com>
2025-03-29 17:32:48 +0700

    29/03

--- backend/src/main/java/com/shopaccgame/config/VNPayConfig.java
+++ backend/src/main/java/com/shopaccgame/config/VNPayConfig.java
@@ -15,8 +15,8 @@ import java.util.*;
 public class VNPayConfig {
 	public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
 	public static String vnp_Returnurl = "/vnpay-payment";
-	public static String vnp_TmnCode = "L8DI5NSO";
-	public static String vnp_HashSecret = "0E9RFYY9Q0HQ3NVAJYM0PT7864919703";
+	public static String vnp_TmnCode = "";
+	public static String vnp_HashSecret = "";
 	public static String vnp_apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 
 	public static String md5(String message) {
